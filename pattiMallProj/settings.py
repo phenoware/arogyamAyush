64a9e79ba239d4fee8a9dc9876225ffe1758961f
phenoware <phenoware@gmail.com>
2023-02-07 00:11:00 +0530

    Updating for server

--- pattiMallProj/settings.py
+++ pattiMallProj/settings.py
@@ -77,23 +77,19 @@ WSGI_APPLICATION = 'pattiMallProj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'arogyamAyush',
+       'HOST': 'arogyamayush.cm8osvjesfbb.ap-south-1.rds.amazonaws.com',
+       'USER': 'arogyamAdmin',
+       'PASSWORD': 'arogyam123Ayush',
+       'PORT': '5432',
+   }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'arogyamAyush',
-#         'USER': 'root',
-#         'PASSWORD':'',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
