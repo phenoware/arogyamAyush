695d827606cbafd6d6709ce6646cf4212c79b49f
phenoware <phenoware@gmail.com>
2023-02-06 10:27:51 +0530

    Dealer Updated

--- pattiMallProj/settings.py
+++ pattiMallProj/settings.py
@@ -142,8 +142,8 @@ EMAIL_HOST_PASSWORD = 'nqtdltuaotgycmmt'
 
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
